d35ea15c2d80a564e6a63927dc72ab38d6a3cc07
juliocpmelo <julio.melo@imd.ufrn.br>
2022-03-28 18:01:39 -0300

    Adicionando "ponto em retângulo"

---
+++ ponto_em_retangulo/src/function.cpp
@@ -0,0 +1,15 @@
+#include "function.h"
+
+/*! 
+ * Verifica se um ponto está dentro de um retângulo.
+ */
+int pt_in_rect( const Ponto &IE, const Ponto &SD, const Ponto &P )
+{
+    // TODO: Adicione seu código aqui.
+
+    return -1;
+}
+
+bool validate_rect( const Ponto&, const Ponto& ){
+    return true;
+}
